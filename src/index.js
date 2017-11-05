5ab7e4481cff448287e16c021c077b7ed62faa1c
Michał Treter <adimus13@gmail.com>
2017-11-05 22:44:37 +0100

    Travis should work now

--- src/index.js
+++ src/index.js
@@ -4,8 +4,8 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render
-(<App>
+ReactDOM.render(
+<App>
     <script src="http://localhost/src/phasTest.js"/>
     </App>, document.getElementById('root'));
 registerServiceWorker();
