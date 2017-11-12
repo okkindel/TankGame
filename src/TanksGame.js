54dade777097f09561cee5c4c921da654f96e9a4
Maciej Hajduk <ebrithil97@gmail.com>
2017-11-12 22:58:30 +0100

    enemies added

--- src/TanksGame.js
+++ src/TanksGame.js
@@ -1,13 +1,14 @@
 import tank_img from './assets/tank.png'
 import wall_img from './assets/wall.png'
 import bullet_img from './assets/bullet.png'
+import enemy_img from './assets/enemy.png'
 
 window.PIXI = require('phaser/build/custom/pixi')
 window.p2 = require('phaser/build/custom/p2')
 window.Phaser = require('phaser/build/custom/phaser-split')
-const Phaser=window.Phaser;
+const Phaser = window.Phaser;
 
-export default function(){
+export default function () {
     var game = new Phaser.Game(800, 600, Phaser.CANVAS, 'phaser', { preload: preload, create: create, update: update });
 
     function preload() {
@@ -15,6 +16,7 @@ export default function(){
         game.load.image('tank', tank_img);
         game.load.image('wall', wall_img);
         game.load.image('bullet', bullet_img);
+        game.load.image('enemy', enemy_img);
     }
 
     var bullets;
@@ -22,11 +24,12 @@ export default function(){
     var bullet;
     var player_dir;
     var player;
+    var enemies;
     var cursors;
     var wall;
 
     function create() {
-        game.physics.startSystem(Phaser.Physics.ARCADE);
+
         game.stage.backgroundColor = '#123';
 
         //player's tank
@@ -34,6 +37,19 @@ export default function(){
         game.physics.enable(player, Phaser.Physics.ARCADE);
         player.body.collideWorldBounds = true;
         player.anchor.set(0.5);
+
+        //enemies
+        enemies = game.add.group();
+        enemies.enableBody = true;
+        enemies.physicsBodyType = Phaser.Physics.ARCADE;
+
+        for (var i = 0; i < 10; i++) {
+            var new_enemy = enemies.create(game.world.randomX, game.world.randomY, 'enemy');
+            new_enemy.name = 'enemy' + i;
+            new_enemy.anchor.set(0.5);
+            new_enemy.angle = game.rnd.integerInRange(0, 360);
+            new_enemy.body.immovable = false;
+        }
 
         //walls
         wall = game.add.sprite(100, 200, 'wall');
@@ -61,7 +77,9 @@ export default function(){
 
     function update() {
 
+        game.physics.arcade.overlap(bullets, enemies, collisionHandler, null, this);
         game.physics.arcade.collide(player, wall);
+        game.physics.arcade.collide(player, enemies);
 
         if (player.alive) {
             player.body.velocity.setTo(0, 0);
@@ -126,8 +144,8 @@ export default function(){
         bullet.kill();
     }
 
-    function collisionHandler(bullet, wall) {
+    function collisionHandler(bullet, enemy) {
         bullet.kill();
-        wall.kill();
+        enemy.kill();
     }
 }
