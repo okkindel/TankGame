1089a0daf5da83ae3c7f977a39257c2262fd0422
Andrzej Rozenfeld <andrzej.rozenfeld@nokia.com>
2017-11-18 23:13:34 +0100

    Problems. Time for fixes.

--- src/App.test.js
+++ src/App.test.js
@@ -40,7 +40,7 @@ describe('App logic test suite', () => {
       });
     });
 
-    it('Entering a mid-password letter changes nothing', () => {
+    it('Entering a password letter adds it', () => {
       const app = new App({ password: 'pass' });
 
       expect(app.state).toEqual({
@@ -53,6 +53,85 @@ describe('App logic test suite', () => {
       expect(app.state).toEqual({
         showApp: false,
         superPrivatePass: 'p'
+      });
+    });
+
+    it('Entering password sets it and showsApp', () => {
+      const app = new App({ password: 'pass' });
+
+      expect(app.state).toEqual({
+        showApp: false,
+        superPrivatePass: ''
+      });
+
+      app.combinationListener({ key: 'p' });
+      app.combinationListener({ key: 'a' });
+      app.combinationListener({ key: 's' });
+      app.combinationListener({ key: 's' });
+
+      expect(app.state).toEqual({
+        showApp: true,
+        superPrivatePass: 'pass'
+      });
+    });
+
+    it('Entering password (and more) sets just the password and showsApp', () => {
+      const app = new App({ password: 'pass' });
+
+      expect(app.state).toEqual({
+        showApp: false,
+        superPrivatePass: ''
+      });
+
+      app.combinationListener({ key: 'p' });
+      app.combinationListener({ key: 'a' });
+      app.combinationListener({ key: 's' });
+      app.combinationListener({ key: 's' });
+      app.combinationListener({ key: 'x' });
+
+      expect(app.state).toEqual({
+        showApp: true,
+        superPrivatePass: 'pass'
+      });
+    });
+
+    it('Beginning with not a password letter showsApp', () => {
+      const app = new App({ password: 'pass' });
+
+      expect(app.state).toEqual({
+        showApp: false,
+        superPrivatePass: ''
+      });
+
+      app.combinationListener({ key: 'x' });
+      app.combinationListener({ key: 'p' });
+      app.combinationListener({ key: 'a' });
+      app.combinationListener({ key: 's' });
+      app.combinationListener({ key: 's' });
+
+      expect(app.state).toEqual({
+        showApp: true,
+        superPrivatePass: 'pass'
+      });
+    });
+
+    it('Beginning with a password letter (twice) showsApp', () => {
+      const app = new App({ password: 'pass' });
+
+      expect(app.state).toEqual({
+        showApp: false,
+        superPrivatePass: ''
+      });
+
+      app.combinationListener({ key: 'a' });
+      app.combinationListener({ key: 'p' });
+      app.combinationListener({ key: 'a' });
+      app.combinationListener({ key: 's' });
+      app.combinationListener({ key: 's' });
+
+      expect(app.state).toEqual({
+        showApp: true,
+        superPrivatePass: 'pass'
       });
     });
   });
