453168953dd8a204f4be2cc83c83ac965443b567
Andrzej Rozenfeld <andrzej.rozenfeld@nokia.com>
2017-11-18 22:38:50 +0100

    First test that tests something :)

--- src/App.test.js
+++ src/App.test.js
@@ -3,10 +3,21 @@ import App from '../src/App';
 
 describe('App logic test suite', () => {
   describe('combinationListener', () => {
-    it('Not much of a test', () => {
+    it('Entering a none-password letter changes nothing', () => {
       const app = new App({ password: 'pass' });
+
+      expect(app.state).toEqual({
+        showApp: false,
+        superPrivatePass: ''
+      });
+
       const event = { key: 'z' };
       app.combinationListener(event);
+
+      expect(app.state).toEqual({
+        showApp: false,
+        superPrivatePass: ''
+      });
     });
   });
 });
