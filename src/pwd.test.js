c41307da5d87b4a015dd74df7fc672e55a7fcd28
Andrzej Rozenfeld <andrzej.rozenfeld@nokia.com>
2017-11-19 01:36:12 +0100

    Complete functionality and an idea (in the skipped test case).

--- src/pwd.test.js
+++ src/pwd.test.js
@@ -1,9 +1,18 @@
 function checkPwd(password, letter, iter = 0) {
   if (!password) { throw new Error('Nu nu. User very bad!'); }
+  if (password.length <= iter) { throw new Error('I\' done already!'); }
+
+  let newIter;
+  if (password[iter] === letter) {
+    newIter = iter + 1;
+  }
+  else {
+    newIter = password[0] === letter ? 1 : 0;
+  }
 
   return {
-    iter: (password[iter] === letter ? iter + 1 : iter),
-    result: false
+    iter: newIter,
+    result: newIter === password.length
   };
 }
 
@@ -39,12 +48,62 @@ describe('Password checker test suite', () => {
 
 
   describe('closured', () => {
-    const check = passwordChecker('pass');
-
-    it('Returns an iterator and a check result', () => {
+    it('full password check', () => {
+      const check = passwordChecker('pass');
       expect(check('p')).toEqual({ iter: 1, result: false });
       expect(check('a')).toEqual({ iter: 2, result: false });
       expect(check('s')).toEqual({ iter: 3, result: false });
+      expect(check('s')).toEqual({ iter: 4, result: true });
+    });
+
+    it('doubled first letter', () => {
+      const check = passwordChecker('pass');
+      expect(check('p')).toEqual({ iter: 1, result: false });
+      expect(check('p')).toEqual({ iter: 1, result: false });
+      expect(check('a')).toEqual({ iter: 2, result: false });
+      expect(check('s')).toEqual({ iter: 3, result: false });
+      expect(check('s')).toEqual({ iter: 4, result: true });
+    });
+
+    it('first letter, error, then ongoing', () => {
+      const check = passwordChecker('pass');
+      expect(check('p')).toEqual({ iter: 1, result: false });
+      expect(check('x')).toEqual({ iter: 0, result: false });
+      expect(check('a')).toEqual({ iter: 0, result: false });
+      expect(check('s')).toEqual({ iter: 0, result: false });
+      expect(check('s')).toEqual({ iter: 0, result: false });
+    });
+
+    it('first letter, second, second, then ongoing', () => {
+      const check = passwordChecker('pass');
+      expect(check('p')).toEqual({ iter: 1, result: false });
+      expect(check('a')).toEqual({ iter: 2, result: false });
+      expect(check('a')).toEqual({ iter: 0, result: false });
+      expect(check('s')).toEqual({ iter: 0, result: false });
+      expect(check('s')).toEqual({ iter: 0, result: false });
+    });
+
+    it('password checker is done', () => {
+      const check = passwordChecker('pass');
+      check('p');
+      check('a');
+      check('s');
+      check('s');
+
+      let threw = false;
+      try {
+        check('x');
+      } catch (error) {
+        expect(error.message).toEqual('I\' done already!');
+        threw = true;
+      }
+      expect(threw).toEqual(true);
+    });
+
+    it.skip('Closure returns just a boolean', () => {
+      const check = passwordChecker('s');
+      expect(check('y')).toEqual(false);
+      expect(check('s')).toEqual(true);
     });
   });
 });
