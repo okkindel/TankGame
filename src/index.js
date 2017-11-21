59e8e5fda2bddd9618129371d86ca16ef064436a
Adrian Mucha <kestarumper8@gmail.com>
2017-11-21 16:39:58 +0100

    pseudomerge

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+
+
+ReactDOM.render(<App password='pass' quitKey='Escape'>
+  <script src='http://localhost/src/phasTest.js' />
+</App>, document.getElementById('root')); // eslint-disable-line react/jsx-closing-tag-location
+
+registerServiceWorker();
