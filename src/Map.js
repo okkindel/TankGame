13b1555fb950e494fe5b5c07ead4bab0f967984c
Michal Treter <adimus13@gmail.com>
2017-11-28 19:24:39 +0100

    Started process of creation test for Map

--- src/Map.js
+++ src/Map.js
@@ -1,34 +1,46 @@
 
 export default class Map{
     constructor(){
-        this.start_point = {};
-        this.enemy_spawn = {};
-        this.walls = [];
-        this.eagle = {};
+            this.start_point = {};
+            this.enemy_spawn = {};
+            this.walls = [];
+            this.eagle = {};
+            this.is_loaded = false;
         
     }
 
     load_map(map){
-        var values = JSON.parse(map);
-        this.start_point = values.start_point;
-        this.enemy_spawn = values.enemy_spawn;
-        this.walls = values.walls;
-        this.eagle = values.eagle;
+        if(!this.is_loaded){
+            var values = JSON.parse(map);
+            this.start_point = values.start_point;
+            this.enemy_spawn = values.enemy_spawn;
+            this.walls = values.walls;
+            this.eagle = values.eagle;
+            is_loaded = true;
+        }
     }
 
     get_enemy_spawn_point(){
-        return this.enemy_spawn;
+        if(this.is_loaded){
+            return this.enemy_spawn;
+        }
     }
 
     get_eagle_point(){
-        return this.eagle;
-    }
+        if(this.is_loaded){
+            return this.eagle;
+        }
+    } 
 
     get_start_point(){
-        return this.start_point;
+        if(this.is_loaded){
+            return this.start_point;
+        }
     }
 
     get_walls_array(){
-        return this.walls;
+        if(this.is_loaded){
+            return this.walls;
+        }
     }
 }
