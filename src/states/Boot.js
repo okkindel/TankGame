d37e4b60e332a8d3089186971489b1d9b8021592
Maciej Hajduk <ebrithil97@gmail.com>
2017-11-28 20:07:35 +0100

    boot menu and repared walls

--- src/states/Boot.js
+++ src/states/Boot.js
@@ -2,14 +2,14 @@ import Phaser from 'phaser'
 import WebFont from 'webfontloader'
 
 export default class extends Phaser.State {
-  init () {
+  init() {
     this.stage.backgroundColor = '#123'
     this.fontsReady = false
     this.fontsLoaded = this.fontsLoaded.bind(this)
     this.buttonCliked = false;
   }
 
-  preload () {
+  preload() {
     WebFont.load({
       google: {
         families: ['Bangers']
@@ -17,29 +17,27 @@ export default class extends Phaser.State {
       active: this.fontsLoaded
     })
 
-    let text = this.add.text(this.world.centerX, this.world.centerY - 100, 'Tank Game', { font: '100px Arial', fill: '#dddddd', align: 'center' })
-    text.anchor.setTo(0.5, 0.5)
-    let button = game.add.button(game.world.centerX - 95, 400, 'button', this.actionOnClick, this, 2, 1, 0);
-
-
-    this.load.image('loaderBg', './assets/images/loader-bg.png')
-    this.load.image('loaderBar', './assets/images/loader-bar.png')
+    this.load.image('button', './assets/button.png')
   }
 
-  render () {
+  create() {
+    let text = this.add.text(this.world.centerX, this.world.centerY - 100, 'Tank Game', { font: '100px Arial', fill: '#dddddd', align: 'center' })
+    text.anchor.setTo(0.5, 0.5)
+    let button = game.add.button(game.world.centerX - 150, 450, 'button', this.actionOnClick, this, 2, 1, 0);
+  }
+
+  render() {
     if (this.buttonCliked) {
       this.state.start('Splash')
     }
   }
 
-  fontsLoaded () {
+  fontsLoaded() {
     this.fontsReady = true
   }
 
 
- actionOnClick () {
-  
-      this.buttonCliked = true;
-  
+  actionOnClick() {
+    this.buttonCliked = true;
   }
 }
