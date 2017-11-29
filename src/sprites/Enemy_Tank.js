6185e55c64f2d3776e1b43ff21e3dcd7ebc5e6b9
Adrian Mucha <kestarumper8@gmail.com>
2017-11-29 11:57:53 +0100

    minor fix

--- src/sprites/Enemy_Tank.js
+++ src/sprites/Enemy_Tank.js
@@ -10,72 +10,44 @@ export default class extends Phaser.Sprite {
       this.body.collideWorldBounds = true,
       this.timeToStep = 0,
       this.direction = 0;
+      this.directions = [];
   }
 
   move(playerX, playerY, eagleX, eagleY, timeNow) {
 
     if (timeNow > this.timeToStep) {
       this.timeToStep = timeNow;
-      this.timeToStep += 2000 * Math.random();
+      this.timeToStep += Math.floor(2000 * Math.random());
 
+        while(this.directions.length > 20)
+          this.directions.shift();
 
       
-      let myArray =[];
-      let distU = Math.sqrt(Math.pow((this.x - playerX), 2) + Math.pow((this.y-1 - playerY), 2));
-      let distD = Math.sqrt(Math.pow((this.x - playerX), 2) + Math.pow((this.y+1 - playerY), 2));
-      let distR = Math.sqrt(Math.pow((this.x+1 - playerX), 2) + Math.pow((this.y - playerY), 2));
-      let distL = Math.sqrt(Math.pow((this.x-1 - playerX), 2) + Math.pow((this.y - playerY), 2));
-      let decision = Math.random();
-      this.direction = Math.floor(Math.random() * (5 - 1)) + 1;
-      if(distD > distU){
-        myArray.push(1);
-        myArray.push(1);
-        myArray.push(1);
-        if(distL < distR){
-          myArray.push(4);
-          myArray.push(4);
-          myArray.push(4);
-        }else{
-          myArray.push(2);
-          myArray.push(2);
-          myArray.push(2);
-        }
-      }else{
-        myArray.push(3);
-        myArray.push(3);
-        myArray.push(3);
-        if(distL < distR){
-          myArray.push(2);
-          myArray.push(2);
-          myArray.push(2);
-        }else{
-          myArray.push(4);
-          myArray.push(4);
-          myArray.push(4);
-        }
+      let distU = Math.abs(this.x - playerX) + Math.abs(this.y-1 - playerY);
+      let distD = Math.abs(this.x - playerX) + Math.abs(this.y+1 - playerY);
+      let distR = Math.abs(this.x+1 - playerX) + Math.abs(this.y - playerY);
+      let distL = Math.abs(this.x-1 - playerX) + Math.abs(this.y - playerY);
+
+      if(playerX > this.x){
+        for(let i=0; i < 3; i++)
+          this.directions.push(1);
+      }
+      if(playerX < this.x){
+        for(let i=0; i < 3; i++)
+          this.directions.push(3);
+      }
+      if(playerY > this.y){
+        for(let i=0; i < 3; i++)
+          this.directions.push(2);
+       }
+      if(playerY < this.y){
+        for(let i=0; i < 3; i++)
+          this.directions.push(4);
       }
 
-      // if(playerX > this.x){
-      //   for(let i=0; i < 3; i++)
-      //     myArray.push(2);
-
-      // }
-      // if(playerX < this.x){
-      //   for(let i=0; i < 3; i++)
-      //     myArray.push(4);
-      // }
-      // if(playerY > this.y){
-      //   for(let i=0; i < 3; i++)
-      //     myArray.push(3);
-      //  }
-      // if(playerY < this.y){
-      //   for(let i=0; i < 3; i++)
-      //     myArray.push(1);
-      // }
-
-      this.direction = myArray[Math.floor(Math.random() * myArray.length)];
+      this.direction = this.directions[Math.floor(Math.random() * this.directions.length)];
       this.body.velocity.setTo(0, 0);
-      console.log(this.direction);
+      console.log(this.directions);
 
     }
     else if (this.direction == 1) {
