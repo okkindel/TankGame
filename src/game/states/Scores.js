da258927884566194322eb3740b21728b7c22823
Maciej Hajduk <ebrithil97@gmail.com>
2018-01-16 12:12:28 +0100

    highscores

--- src/game/states/Scores.js
+++ src/game/states/Scores.js
@@ -27,8 +27,22 @@ export default class extends Phaser.State {
     this.game.score.resetScore();
   }
 
+  constructor() {
+    super();
+    this.users = { adi: 10, tete: 20, maku: 30, walcu: 40, hajduk: 50 };
+  }
+
   create() {
-    let button = this.game.add.button(this.game.world.centerX - 150, 500, 'button', this.actionOnClick, this, 2, 1, 0);
+    let text = this.add.text(this.world.centerX, this.world.centerY - 240, 'Highscores', { font: '100px Sheriff', fill: '#fff', align: 'center' })
+    text.anchor.setTo(0.5, 0.5)
+    let button = this.game.add.button(this.game.world.centerX - 150, 520, 'button', this.actionOnClick, this, 2, 1, 0);
+    let i = 0;
+    for (let key in this.users) {
+      let text = this.add.text(this.world.centerX, 250 + 50 * i, i + ':              ' + key + ':  ' + this.users[key],
+        { font: '20px', fill: '#fff', align: 'center' })
+      text.anchor.setTo(0.5, 0.5)
+      i += 1
+    }
   }
 
   render() {
